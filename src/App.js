539eb3888e6cc6900c09176ac67ea39dfad1e817
seoyeon-baek <w2032@e-mirim.hs.kr>
2022-03-17 00:50:25 +0900

    Fix: node, package-lock 재설치
    module not found error

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from "./logo.svg";
 import "./App.css";
 import {BrowserRouter, Routes, Route} from "react-router-dom";
-import MapPage from "./components/map";
+import {MapPage} from "./components/map";
 
 function App() {
   return (
