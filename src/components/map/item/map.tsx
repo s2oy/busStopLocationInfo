290b25ecc0c0a8d0ae6ae6d69bfe56dc02f2fc7c
seoyeon-baek <w2032@e-mirim.hs.kr>
2022-03-19 00:10:11 +0900

    Add: marker 표시

--- src/components/map/item/map.tsx
+++ src/components/map/item/map.tsx
@@ -11,16 +11,24 @@ const Map = () => {
   useEffect(() => {
     let container = document.getElementById("map");
     let options = {
-      center: new window.kakao.maps.LatLng(35.85133, 127.734086),
-      level: 13,
+      center: new window.kakao.maps.LatLng(33.450701, 126.570667),
+      level: 3,
     };
     let map = new window.kakao.maps.Map(container, options);
+    let markerPosition = new window.kakao.maps.LatLng(33.450701, 126.570667);
+    let marker = new window.kakao.maps.Marker({
+      position: markerPosition,
+    });
+    marker.setMap(map);
     // console.log("loading kakaomap");
   }, []);
 
   return (
     <S.MapContainer>
-      <div id="map" style={{width: "400px", height: "500px"}}></div>
+      <div
+        id="map"
+        style={{width: "100vh", height: "100vh", margin: "0 auto"}}
+      ></div>
     </S.MapContainer>
   );
 };
