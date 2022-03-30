ee81dea6f2fbd7fc22d7adb4cb12104012d12f26
seoyeon-baek <w2032@e-mirim.hs.kr>
2022-03-30 22:09:56 +0900

    Add: 사용자 현재 위치정보 가져와서 뿌려줌

--- src/components/map/item/map.tsx
+++ src/components/map/item/map.tsx
@@ -10,52 +10,57 @@ declare global {
 const Map = () => {
   //map 불러옴
   useEffect(() => {
-    let container = document.getElementById("map");
-    let options = {
+    let mapContainer = document.getElementById("map");
+    let mapOption = {
       center: new window.kakao.maps.LatLng(
         37.466806528974935,
         126.93216211295136
       ),
       level: 3,
     };
-    let map = new window.kakao.maps.Map(container, options);
+    let map = new window.kakao.maps.Map(mapContainer, mapOption);
 
-    //marker
-    let markerPosition = new window.kakao.maps.LatLng(
-      37.466806528974935,
-      126.93216211295136
-    );
-    let marker = new window.kakao.maps.Marker({
-      position: markerPosition,
-      clickable: true, //marker 클릭했을 때 지도 클릭이벤트 방지
-    });
-    marker.setMap(map);
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function (position: any) {
+        let lat = position.coords.latitude; //위도
+        let lon = position.coords.longitude; //경도
 
-    let iwContent = "미림마이스터고";
-    let iwRemoveable = true;
+        let locPosition = new window.kakao.maps.LatLng(lat, lon),
+          message = '<div style="padding:5px">여기에 계신가요?</div>';
 
-    let infowindow = new window.kakao.maps.InfoWindow({
-      content: iwContent,
-      removable: iwRemoveable,
-    });
+        displayMarker(locPosition, message);
+      });
+    } else {
+      let locPosition = new window.kakao.mpas.LatLng(
+          37.466806528974935,
+          126.93216211295136
+        ),
+        message = "geolocation을 사용할 수 없습니다.";
+    }
 
-    window.kakao.maps.event.addListener(marker, "click", function () {
-      infowindow.open(map, marker);
-    });
+    function displayMarker(locPosition: any, message: any) {
+      let marker = new window.kakao.maps.Marker({
+        map: map,
+        position: locPosition,
+        clickable: true, //marker 클릭했을 때 지도 클릭이벤트 방지
+      });
+
+      let iwContent = message;
+      let iwRemoveable = true;
+
+      let infowindow = new window.kakao.maps.InfoWindow({
+        content: iwContent,
+        removable: iwRemoveable,
+      });
+
+      window.kakao.maps.event.addListener(marker, "click", function () {
+        infowindow.open(map, marker);
+      });
+
+      map.setCenter(locPosition);
+      marker.setMap(map);
+    }
   }, []);
-
-  const url =
-    "https://api.odcloud.kr/api/15081069/v1/uddi:f0ac92c3-e9f0-4d34-9fae-8751ba6e53f9?page=1&perPage=10&serviceKey=9Tnec%2BHetTZ23faMm5uSo19iuo4ZbUQVfUxMVHMkp4v%2BQmj%2FHtHuqXsDAkekMfe8bBHxd7T99xPZTStJQX5ppQ%3D%3D";
-
-  fetch(url)
-    .then(res => res.json())
-    .then(myJson => {
-      document.getElementById("data")!.innerText = JSON.stringify(
-        myJson,
-        null,
-        1
-      );
-    });
 
   return (
     <S.MapContainer>
@@ -69,3 +74,6 @@ const Map = () => {
 };
 
 export default Map;
+function doSomethig(latitude: number, longitude: number) {
+  throw new Error("Function not implemented.");
+}
